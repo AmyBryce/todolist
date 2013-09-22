02150c5739ed72fb84c93bdb85f752ebc5434f9c
AmyBryce <a.bryce9@gmail.com>
2013-09-22 12:29:06 -0700

    Added strike-through feature to a completed task.
    Used JQuery to implement strike-through CSS code to a task that has been
    completed indicated by checking the checkbox.

--- js/todo.js
+++ js/todo.js
@@ -8,9 +8,9 @@ var jquerytodo = function() {
 		var task = $jquerytask.html();
 		return $(task);
 	};
-
+	
 	$input.keypress(function(e) {
-		if (e.which == 13) { //Enter key pressed
+		if (e.which == 13) { // Enter key pressed
 			$addbutton.click();
 		}
 	});
@@ -23,6 +23,14 @@ var jquerytodo = function() {
 			$input.val('');
 			$task.find(".taskdeletebutton").click(function() {
 				$task.remove();
+			});
+			$task.find("input[type=checkbox]").click(function() {
+				var $tasktext = $task.find(".tasktext");
+				if ($(this).is(":checked") === true) {
+					$tasktext.addClass("tasktextcompleted");
+				} else {
+					$tasktext.removeClass("tasktextcompleted");
+				}
 			});
 		}
 	});
